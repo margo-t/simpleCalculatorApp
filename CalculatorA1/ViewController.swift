0dfb8951e0f662d5c86a4455a01e5c26e84cbf25
MargoT <mtereshc@gmail.com>
2017-01-23 15:24:08 -0500

    Final v1:percentage, root, power buttons working

--- CalculatorA1/ViewController.swift
+++ CalculatorA1/ViewController.swift
@@ -2,9 +2,10 @@
 //  ViewController.swift
 //  CalculatorA1
 //
-//  Created by Margo T on 2017-01-16.
+//  Created by Margo Tereshchenkova on 2017-01-16.
 //  Copyright © 2017 margot.centennial. All rights reserved.
 //
+//  Basic Calculator: perform simple operations with numbers
 
 import UIKit
 
@@ -12,6 +13,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var terminalView: UILabel!
     
+    /////////////////////
+    //Declare variables//
+    /////////////////////
     
     var currentNumber = ""
     var currentOperation = Operation.clear
@@ -19,7 +23,6 @@ class ViewController: UIViewController {
     var secondValue = "0"
     var result = ""
     var float = false
-    
     
     enum Operation: String {
         
@@ -29,12 +32,14 @@ class ViewController: UIViewController {
         case division = "/"
         case clear = "clear"
         case clean = "clean"
-        case floating = "."
+        case power = "power"
         }
     
-
-    // Number touched Actions
+    /////////////////////////////////////////////
+    // Activities when Number Button is pressed//
+    /////////////////////////////////////////////
     
+        //Simple digits
     @IBAction func numberTouched(sender: UIButton){
         if let symbol  = sender.currentTitle {
             if symbol == "." && !float {
@@ -52,10 +57,29 @@ class ViewController: UIViewController {
             
         }
     }
+        //Special numbers
+    @IBAction func specialSymbol(sender: UIButton){
+            if let mathSymbol  = sender.currentTitle {
+            if mathSymbol == "π" {
+                currentNumber = "\(M_PI)"
+                terminalView.text = currentNumber
+                float=true
+            }
+            else if mathSymbol == "e" {
+                currentNumber = "\(M_E)"
+                terminalView.text = currentNumber
+                float=true
+            }
+            
+            printAllInstances()
+        }
+    }
     
+    /////////////////////////
+    // Operation Activities//
+    /////////////////////////
     
-    // Operation Actions
-    
+        //---Basic operations with two variables involved----
     @IBAction func onAddition(sender: UIButton){
         processOperation(operation: .addition)
         printAllInstances()
@@ -79,13 +103,12 @@ class ViewController: UIViewController {
         printAllInstances()
     }
     
-    @IBAction func onNegative(sender: UIButton){
-        if currentNumber != "" {
-            currentNumber="\((Double(currentNumber)! * (-1)).format)"
-            terminalView.text = currentNumber}
+    @IBAction func onPower(sender: UIButton){
+        processOperation(operation: .power)
         printAllInstances()
     }
     
+    //----Clear all operations and Variables----
     @IBAction func onClean(sender: UIButton){
         currentNumber = ""
         firstValue = "0"
@@ -97,38 +120,56 @@ class ViewController: UIViewController {
         printAllInstances()
     }
     
-    @IBAction func specialSymbol(sender: UIButton){
-        if let mathSymbol  = sender.currentTitle {
-            if mathSymbol == "π" {
-                currentNumber = "\(M_PI)"
-                terminalView.text = currentNumber
-            }
-            else if mathSymbol == "e" {
-                currentNumber = "\(M_E)"
-                terminalView.text = currentNumber
-            }
+        //----Calculate Percentage of entered number from Previous one
+        //this operation requies different logic and requirements than in "processOperation"
+    @IBAction func onPercentage(sender: UIButton){
+        //processOperation(operation: .percentage)
+        if secondValue != "0" && currentOperation != Operation.clear && currentNumber != ""{
             
+            currentNumber = "\((Double(secondValue)! * (Double(currentNumber)! / 100)).format)"
+            terminalView.text = currentNumber
             printAllInstances()
         }
     }
     
+        //----Change sign of current value
+        //Changes should be made only for one variable - 'currentNumber'
+    @IBAction func onNegative(sender: UIButton){
+        if currentNumber != "" || secondValue != "0"{
+            if currentNumber != "" {
+                currentNumber="\((Double(currentNumber)! * (-1)).format)"
+                terminalView.text = currentNumber}
+            else if secondValue != "0" {
+                currentNumber="\((Double(secondValue)! * (-1)).format)"
+                terminalView.text = currentNumber
+            }
+        }
+        printAllInstances()
+    }
     
-    func printAllInstances(){
-        
-        print("currentNumber:"+currentNumber)
-        print("firstValue:"+firstValue)
-        print("secondValue:"+secondValue)
-        print("result:"+result)
-        print("float: \(float)")
-        print("terminalView:"+terminalView.text!)
-        print("--------------------------------")
+        //----Take root of current value
+        //Changes should be made only for one variable - 'currentNumber'
+    @IBAction func onRoot(sender: UIButton){
+        if currentNumber != "" || secondValue != "0"{
+            if currentNumber != "" {
+                currentNumber="\(sqrt(Double(currentNumber)!).format)"
+                terminalView.text = currentNumber}
+            else if secondValue != "0" {
+                currentNumber="\(sqrt(Double(secondValue)!).format)"
+                terminalView.text = currentNumber
+            }
+        }
+        printAllInstances()
         }
     
+    
+    
+    
+    
+    //logic to process operation with two variables
     func processOperation(operation: Operation){
         
-        
         if currentOperation != Operation.clear{
-            
             
             //entering operators twice without entering a number in between
             if currentNumber != "" {
@@ -148,6 +189,9 @@ class ViewController: UIViewController {
                 else if currentOperation == Operation.multiplication {
                     result = "\((Double(secondValue)! * Double(firstValue)!).format)"
                 }
+                else if currentOperation == Operation.power {
+                    result = "\((pow(Double(secondValue)!, Double(firstValue)!)).format)"
+                }
                 
                 secondValue = result
                 terminalView.text = result
@@ -163,17 +207,30 @@ class ViewController: UIViewController {
             currentNumber = ""
             currentOperation = operation
             
-        
             }
         
         }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //Keep log of all var updates
+    func printAllInstances(){
+        
+        print("currentNumber:"+currentNumber)
+        print("firstValue:"+firstValue)
+        print("secondValue:"+secondValue)
+        print("result:"+result)
+        print("float: \(float)")
+        print("terminalView:"+terminalView.text!)
+        print("--------------------------------")
     }
+    
+
 
 }
+
+
+    //How to remove decimal part from number: (solution found - http://stackoverflow.com/questions/31390466/swift-how-to-remove-a-decimal-from-a-float-if-the-decimal-is-equal-to-0)
     extension Double {
         var format: String {
         return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
