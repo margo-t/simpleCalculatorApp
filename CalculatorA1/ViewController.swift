6293ee40a09687f81d3d6d20a7dfbba7b65af0f9
MargoT <mtereshc@gmail.com>
2017-01-23 11:03:27 -0500

    Decimal point functionality added

--- CalculatorA1/ViewController.swift
+++ CalculatorA1/ViewController.swift
@@ -15,10 +15,10 @@ class ViewController: UIViewController {
     
     var currentNumber = ""
     var currentOperation = Operation.clear
-    
     var firstValue = "0"
     var secondValue = "0"
     var result = ""
+    var float = false
     
     
     enum Operation: String {
@@ -29,14 +29,28 @@ class ViewController: UIViewController {
         case division = "/"
         case clear = "clear"
         case clean = "clean"
+        case floating = "."
         }
     
 
     // Number touched Actions
     
     @IBAction func numberTouched(sender: UIButton){
+        if let symbol  = sender.currentTitle {
+            if symbol == "." && !float {
+                currentNumber += "."
+                terminalView.text = currentNumber
+                float=true
+                printAllInstances()}
+
+            else if symbol != "."{
+        
         currentNumber += "\(sender.tag)"
         terminalView.text = currentNumber
+            printAllInstances()
+                }
+            
+        }
     }
     
     
@@ -65,15 +79,39 @@ class ViewController: UIViewController {
         printAllInstances()
     }
     
+    @IBAction func onNegative(sender: UIButton){
+        if currentNumber != "" {
+            currentNumber="\((Double(currentNumber)! * (-1)).format)"
+            terminalView.text = currentNumber}
+        printAllInstances()
+    }
+    
     @IBAction func onClean(sender: UIButton){
         currentNumber = ""
         firstValue = "0"
         secondValue = "0"
         result = ""
+        float=false
         terminalView.text = "0"
         currentOperation = Operation.clear
         printAllInstances()
     }
+    
+    @IBAction func specialSymbol(sender: UIButton){
+        if let mathSymbol  = sender.currentTitle {
+            if mathSymbol == "π" {
+                currentNumber = "\(M_PI)"
+                terminalView.text = currentNumber
+            }
+            else if mathSymbol == "e" {
+                currentNumber = "\(M_E)"
+                terminalView.text = currentNumber
+            }
+            
+            printAllInstances()
+        }
+    }
+    
     
     func printAllInstances(){
         
@@ -81,18 +119,20 @@ class ViewController: UIViewController {
         print("firstValue:"+firstValue)
         print("secondValue:"+secondValue)
         print("result:"+result)
+        print("float: \(float)")
         print("terminalView:"+terminalView.text!)
         print("--------------------------------")
         }
     
     func processOperation(operation: Operation){
         
-        //TODO: fix if only one number was entered
         
         if currentOperation != Operation.clear{
             
+            
             //entering operators twice without entering a number in between
             if currentNumber != "" {
+                
                 firstValue = currentNumber
                 currentNumber = ""
                 
@@ -111,6 +151,7 @@ class ViewController: UIViewController {
                 
                 secondValue = result
                 terminalView.text = result
+                float=false
                 }
             
             currentOperation = operation
